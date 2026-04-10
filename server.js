b28ad3a4b72a55dd23a60309c98b332b9d011b52
Pavan Praneeth <varipavan415@gmail.com>
2026-04-10 22:39:11 +0530

    Fix: Use absolute path for static files, update vercel.json routing

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const http = require('http');
+const path = require('path');
 const { Server } = require('socket.io');
 const {
   BOARD_LAYOUT, createDeck, getHandSize, createEmptyBoard,
@@ -11,7 +12,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, { cors: { origin: '*' } });
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 const rooms = {};
 
